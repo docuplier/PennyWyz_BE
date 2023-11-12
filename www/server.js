3e6de2225926c0225ade8038035542964271ea77
chibuike umechukwu <umebuike@gmail.com>
2023-11-12 10:27:57 +0100

    ...

--- www/server.js
+++ www/server.js
@@ -2,7 +2,6 @@ import http from 'http';
 import app from '../src/index.js';
 import db from '../src/database/models/index.js';
 import logger from '../src/utils/logger.js';
-import seedProducts from '../src/database/seeders/index.js';
 
 const server = http.createServer(app);
 
@@ -11,7 +10,6 @@ const port = process.env.PORT || 8080;
 db.connection
   .sync({ force: +process.argv[2] === 1 })
   .then(() => logger.info('Database Connected.'))
-  .then(() => seedProducts())
   .then(() => {
     server.listen(port, () => {
       logger.info(`Listening on PORT: ${port}`);
